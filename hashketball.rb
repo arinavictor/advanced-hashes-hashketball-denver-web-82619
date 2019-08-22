2af8c99563ee832ec326c7e8aa2b3c0e06f8ef0a
Arina Victor <arinavictor@yahoo.com>
2019-08-22 23:59:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,7 +56,7 @@ end
 def player_numbers(team_name)
   game_hash.each do |location, keys|
     if keys[:team_name] == team_name
-      return keys[:players].collect { |players| player[:number] }
+      return keys[:players].collect { |player| player[:number] }
     end
   end 
 end 
