ae5f792fbe238d9f490f155ce44f3b49ecdb7fe2
Arina Victor <arinavictor@yahoo.com>
2019-08-22 23:52:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -43,7 +43,7 @@ end
 
 def team_colors(team_name)
   game_hash.each do |location, keys|
-      return game_hash[location][:colors] if team[:team_name] == team_name
+      return game_hash[location][:colors] if keys[:team_name] == team_name
     end 
   end 
 
