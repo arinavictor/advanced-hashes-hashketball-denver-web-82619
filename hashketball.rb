aced6ed99d02ea1e368d3d2a399aa37fb4b29dc6
Arina Victor <arinavictor@yahoo.com>
2019-08-22 23:37:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,11 +27,7 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |location, team|
-    team.each do |attribute, data|
-      if attribute == :players
-      data.each do |player|
-        
-        if player[:name] == player_name
+    
           
 end 
   
