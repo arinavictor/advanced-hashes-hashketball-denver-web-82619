58c9dc8334fbff9fc244fa49a62d47fd8bc24930
Arina Victor <arinavictor@yahoo.com>
2019-08-23 02:08:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ def player_with_longest_name
       player_name = player[:player_name]
       if player_name.length > most_letters
         most_letters = player_name.length 
-        longest_name = plater[:player_name]
+        longest_name = player[:player_name]
       end 
     end 
   end 
