ca196bf138734a4dd712d4f82999acbf78b2bb53
Arina Victor <arinavictor@yahoo.com>
2019-08-23 00:07:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,7 +65,7 @@ end
 def player_stats(player_name)
   game_hash.each do |location, keys|
   keys[:players].each do |player_name, value|
-    if player_name == name 
+    if player_name == player_name 
       return value 
     end 
   end 
