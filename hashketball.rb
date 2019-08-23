253638f87b45a3d20b2030f7b927573da26e9826
Arina Victor <arinavictor@yahoo.com>
2019-08-23 00:42:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -77,7 +77,8 @@ def big_shoe_rebounds
   rebounds = 0 
   game_hash.each do |location, keys|
     keys[:players].each do |player_name, value|
-      if value[:big_shoes]
+      if value[:shoes] > big_shoes
+        return 
       
       
     
